f507fac80cdf2fc81f0224b667be47d4e0d171d8
l0c4lh0s7 <sumit.bhakt@mavenwave.com>
2018-10-15 15:26:26 +0530

    Added getters and setter to exceptionDetail

--- src/main/java/sumit/hrms01/exceptions/ApplicantNotFoundException.java
+++ src/main/java/sumit/hrms01/exceptions/ApplicantNotFoundException.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ApplicantNotFoundException extends RuntimeException{
 
+	private static final long serialVersionUID = 1L;
+
 	public ApplicantNotFoundException(String message) {
 		super(message);
 	}
--- src/main/java/sumit/hrms01/exceptions/ExceptionDetail.java
+++ src/main/java/sumit/hrms01/exceptions/ExceptionDetail.java
@@ -12,6 +12,24 @@ public class ExceptionDetail {
 		this.message = message;
 		this.details = details;
 	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
 	
 	
 }
