7d6fbc3d19058c2389b80bc4a1dffc99bd566414
l0c4lh0s7 <sumit.bhakt@mavenwave.com>
2018-11-23 14:08:35 +0530

    removed unused imports

--- src/main/java/sumit/hrms01/service/CredentialService.java
+++ src/main/java/sumit/hrms01/service/CredentialService.java
@@ -1,7 +1,6 @@
 package sumit.hrms01.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import sumit.hrms01.model.Credential;
--- src/main/java/sumit/hrms01/service/UserService.java
+++ src/main/java/sumit/hrms01/service/UserService.java
@@ -6,10 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import sumit.hrms01.exceptions.ApplicantNotFoundException;
